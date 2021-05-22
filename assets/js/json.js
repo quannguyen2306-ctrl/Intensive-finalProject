571c465e0703e156d3320e62ec55b1c64b83149c
quannguyen2306-ctrl <65434894+quannguyen2306-ctrl@users.noreply.github.com>
2021-05-22 20:46:40 +0700

    finalProject week 2

---
+++ assets/js/json.js
@@ -0,0 +1,44 @@
+let example = {
+    name: "Quan",
+    email: "example.vn@gmail.com",
+};
+
+let voice_AI = {
+    voice: "https bruh",
+    id: "play",
+}
+
+let books = {
+    cover: "some lmao image's link",
+    content: "https dark",
+    description: "This book is bủh bủh lmao",
+    view: [],
+    like: [],
+    dislike: [],
+} 
+
+let users = {
+    name: "Your Name",
+    email: "example.lmao@gmail.com",
+    avatar: ">:))",
+    description: "Hi! I'am superman"
+}
+
+let voice_recorders = {
+    voice: "https something",
+    
+}
+/**
+ * voice-AI
+Thuộc tính: giọng nói (string)
+
+books
+Thuộc tính: bìa sách(img), nội dung (sound, string), giới thiệu(string), số lượt xem (array), số lượt thích (array), số lượt không thích (array)
+
+users 
+Thuộc tính: tên (string), email (string), avatar(image), giới thiệu (string)
+
+voice-recorders
+Thuộc tính: giọng nói (string), bìa (image), thông tin tác giả (map), số lượt thích-xem-không thích (array)
+
+ */
